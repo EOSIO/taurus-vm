8e9eef3824bc1a2614a51ad3e7d4d4d34a418da7
EOSIO-Taurus <eosio-taurus@bullish.com>
2023-06-14 11:15:22 +0800

    EOSIO-Taurus changes

--- .cicd/platforms/macos-10.15.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eou pipefail
-VERSION=1
-
-brew update && brew upgrade
-brew install git cmake
--- include/eosio/vm/function_traits.hpp
+++ include/eosio/vm/function_traits.hpp
@@ -53,11 +53,6 @@ namespace eosio { namespace vm {
       template <typename T>
       constexpr bool pass_type() { return true; }
 
-      template <typename>
-      constexpr bool is_callable_impl(...) {
-        return false;
-      }
-
       template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
       template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
@@ -71,9 +66,6 @@ namespace eosio { namespace vm {
       template <typename T, typename U>
       inline constexpr U&& make_dependent(U&& u) { return static_cast<U&&>(u); }
    }
-
-   template <auto FN>
-   inline constexpr static bool is_callable_v = EOS_VM_HAS_MEMBER(AUTO_PARAM_WORKAROUND(FN), operator());
 
    template <typename F>
    constexpr bool is_callable(F&& fn) { return EOS_VM_HAS_MEMBER(fn, operator()); }
@@ -102,7 +94,7 @@ namespace eosio { namespace vm {
                                                                std::tuple<std::conditional_t<Decay, std::decay_t<Args>, Args>...>>;
       template <bool Decay, typename F>
       constexpr auto get_types(F&& fn) {
-         if constexpr (is_callable_v<decltype(fn)>)
+         if constexpr (is_callable(fn))
             return get_types<Decay>(&F::operator());
          else
             return get_types<Decay>(fn);
@@ -144,7 +136,7 @@ namespace eosio { namespace vm {
       constexpr auto parameters_from_impl(R(Cls::*)(Args...)const &&) ->  pack_from_t<N, Args...>;
       template <std::size_t N, typename F>
       constexpr auto parameters_from_impl(F&& fn) {
-         if constexpr (is_callable_v<decltype(fn)>)
+         if constexpr (is_callable(fn))
             return parameters_from_impl<N>(&F::operator());
          else
             return parameters_from_impl<N>(fn);
